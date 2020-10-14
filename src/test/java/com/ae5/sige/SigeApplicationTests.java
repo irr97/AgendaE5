ee124cde532878c85a909590f401c80b9f8bfcd3
RamonDiazRodrigo <Ramon.Diaz2@alu.uclm.es>
2020-10-14 18:01:36 +0200

    Spring

---
+++ src/test/java/com/ae5/sige/SigeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ae5.sige;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
